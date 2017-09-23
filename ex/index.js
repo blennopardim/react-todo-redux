8999ceaaa5c9ae81925a9fe20582f03905d5db62
Blenno Pardim <blenno@live.com>
2017-09-22 21:48:47 -0300

    Add babel loader and presset es2015

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,6 @@
-console.log('Webpack')
+const logger = require('./logger')
+import Pessoa from './pessoa'
+
+logger.info('Usando o padrão CommonJS!')
+const pessoa = new Pessoa('Blenno')
+console.log(pessoa.toString())
