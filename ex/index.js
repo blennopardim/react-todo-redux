170736ae263ebbcc6ef5e09936b73a2cb31bb4b1
Blenno Pardim <blenno@live.com>
2017-09-22 22:01:13 -0300

    Add spead plugin babel

--- ex/index.js
+++ ex/index.js
@@ -1,6 +1,21 @@
 const logger = require('./logger')
 import Pessoa from './pessoa'
 
+const product = {
+    name: 'Pen Bic Black',
+    price: 1.90,
+    discount: 0.05
+}
+
+function clone(object) {
+    return { ...object}
+}
+
+const newProduct = clone(product)
+newProduct.name = 'Pen Bic Blue'
+
+console.log(product, newProduct)
+
 logger.info('Usando o padrão CommonJS!')
 const pessoa = new Pessoa('Blenno')
 console.log(pessoa.toString())
