ac4023e3b0081965ec24434c4e40897dda77fd9a
Blenno Pardim <blenno@live.com>
2017-09-24 16:11:25 -0300

    Example components react

---
+++ ex/component.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default props => (
+    <h1 class="{props.class}">{props.value}</h1>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './ex/index.js',
+    entry: './ex/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
@@ -10,15 +11,24 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
     module: {
         loaders: [{
-            test: /.js?$/,
+            test: /.jsx?$/,
             loader: 'babel-loader',
             exclude: '/node-modules/',
             query: {
-                presets: ['es2015'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
         }]
     }
 }
