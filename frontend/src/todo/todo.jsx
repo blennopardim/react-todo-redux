c78728d06b477c0cb700f2c030bf9bdf00732ff6
Blenno Pardim <blenno@live.com>
2017-09-25 00:00:00 -0300

    Todo router and init todoComponents

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+export default class Todo extends React.Component {
+    render() {
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm />
+                <TodoList />
+            </div>
+        )
+    }
+}
