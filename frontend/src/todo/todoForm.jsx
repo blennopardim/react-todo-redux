62ea4bc392a8b1a8649f5c1464abdd77343a6148
Blenno Pardim <blenno@live.com>
2017-09-30 00:37:30 -0300

    First version todo work

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>Form</h1>
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key == 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd() 
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+    return (
+        <div role='form' className='todoForm row'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control' type="text"
+                onChange={props.handleChange}
+                onKeyUp={keyHandler}
+                placeholder='Adicione uma tarefa' value={props.description} />
+        </Grid>
+        
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='fa-plus' 
+                onClick={props.handleAdd}></IconButton>
+            <IconButton style='info' icon='fa-search' 
+                onClick={props.handleSearch}></IconButton>
+            <IconButton style='default' icon='fa-close' 
+                onClick={props.handleClear}></IconButton>
+        </Grid>
     </div>
-)
+    )
+}
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+    
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo =>(
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone': ''}>{todo.description}</td>
+                <td>
+                    <IconButton style='success' icon='fa-check' hide={todo.done} onClick={() => props.handleMarkAsDone(todo)} />
+                    <IconButton style='warning' icon='fa-undo' hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)} />
+                    <IconButton style='danger' icon='fa-trash-o' hide={!todo.done} onClick={() => props.handleRemove(todo)} />
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className='tableActions'>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
